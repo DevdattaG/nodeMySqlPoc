7f366038c909e4322e06204d72a54c3b2b7f49b4
DevdattaG <devdatta.godbole777@gmail.com>
2018-01-15 13:16:40 +0530

    Adding basic structure for database initialization. Adding sequelize with MySql

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 var express = require('express');
+var passport = require('passport');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var models = require("./app/models");
+var env = require('dotenv').load();
 var app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(session({secret : 'keyboard cat', resave : true, saveUninitialized:true}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', function(req,res){
     res.send('Welcome to node + mysql + passport + sequelize');
@@ -11,4 +21,10 @@ app.listen(5000, function(err){
     }else{
         console.log("error " + err);
     }
+});
+
+models.sequelize.sync().then(function(){
+    console.log("Good to go !!! Database all set");    
+}).catch(function(err){
+    console.log("Encountered error" + err);
 });
